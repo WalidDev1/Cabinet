7c07117896508ab1a5be17201045fb2949bd0e2f
walidS <walidsousside@gmail.com>
2020-12-23 14:51:04 +0100

    First commit

---
+++ src/cabinet/Model/Patient.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cabinet.Model;
+
+import java.util.Date;
+
+/**
+ *
+ * @author admin
+ */
+public class Patient {
+    String nom ;
+    String prenom;
+    Date date_debut_consultation;
+}
